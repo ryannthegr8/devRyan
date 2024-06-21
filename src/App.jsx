e9e63e5f0074efc46af9018d89c635e54ebba32f
ryannthegr8 <mmbaleryan@gmail.com>
2024-06-21 03:14:28 +0300

    lucideData

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./style/style.css";
 import "./style/icons.css";
 
+//Lucid Icons
+import "./assets//libs/lucide/umd/lucide.min.js";
+
 //Pages
 import { Home } from "./pages";
 
