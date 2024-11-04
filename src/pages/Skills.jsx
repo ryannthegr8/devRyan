d5b2b6882ea2cf3b0e12dd760b98115cf1c26bbb
ryannthegr8 <mmbaleryan@gmail.com>
2024-11-04 16:10:56 +0300

    skills page Setup

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,115 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import Me from "../assets/me.jpeg";
+//Components
+import { Nav, Footer } from "../components";
+
+//Pages and images
+import {
+  imageA,
+  imageB,
+  imageC,
+  imageD,
+  imageE,
+  imageF,
+  imageG,
+  imageH,
+  imageK,
+  imageI,
+  imageJ,
+  imageL,
+  imageM,
+  imageN,
+  imageO,
+  imageP,
+  imageQ,
+} from "../pages";
+
+import { TbEyePlus, TbBulb } from "react-icons/tb";
+
+import aos from "aos";
+import "aos/dist/aos.css";
 
 function Skills() {
-  return <div>Skills</div>;
+  useEffect(() => {
+    aos.init();
+  });
+  return (
+    <>
+      <body class="bg-slate-900">
+        <div>
+          <div class="fixed top-0 -z-10">
+            <div class="blur-[200px] w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] rounded-full bg-gradient-to-tl from-red-600/40 to-pink-600/40"></div>
+          </div>
+          <div class="fixed top-0 end-0 -z-10">
+            <div class="blur-[200px] w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] rounded-full bg-gradient-to-tl from-red-600/40 to-pink-600/40"></div>
+          </div>
+          <div class="fixed inset-0 hidden lg:flex items-center justify-center -z-10">
+            <div class="blur-[200px] w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] rounded-full bg-gradient-to-tl from-red-600/40 to-pink-600/40"></div>
+          </div>
+          <div class="fixed bottom-0 start-0 -z-10">
+            <div class="blur-[200px] w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] rounded-full bg-gradient-to-tl from-red-600/40 to-pink-600/40"></div>
+          </div>
+          <div class="fixed bottom-0 end-0 -z-10">
+            <div class="blur-[200px] w-[300px] h-[300px] lg:w-[500px] lg:h-[500px] rounded-full bg-gradient-to-tl from-red-600/40 to-pink-600/40"></div>
+          </div>
+        </div>
+        {/* <!-- end body bg gradient --> */}
+
+        {/* <!-- Navbar Start --> */}
+        <header id="navbar-sticky" class="navbar">
+          <div class="container">
+            <Nav />
+          </div>
+        </header>
+        {/* <!-- Navbar End --> */}
+
+        {/* <!-- Hero Section Start --> */}
+        <section
+          id="home"
+          class="relative overflow-hidden pt-40 pb-20"
+          data-aos="zoom-out"
+          data-aos-easing="ease"
+          data-aos-duration="1000"
+        >
+          <div class="absolute h-14 w-14 bg-primary/10 top-1/2 start-80 -z-1 rounded-2xl rounded-tl-none rounded-br-none animate-[spin_10s_linear_infinite]"></div>
+          <div class="absolute h-14 w-14 bg-primary/20 top-1/2 end-80 -z-1 rounded-full animate-ping"></div>
+          <div class="container">
+            <div class="text-center">
+              <div class="flex justify-center mt-6">
+                <div class="max-w-2xl">
+                  <h1 class="text-5xl/tight text-default-100 font-medium mb-6">
+                    My Skills.
+                  </h1>
+                  <p class="text-base text-default-300 font-medium lg:max-w-md mx-auto">
+                    What I Actually Do.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <!-- Container End --> */}
+        </section>
+        {/* <!-- Hero end  --> */}
+
+        {/* Page content */}
+
+        {/* End of Page content */}
+
+        <Footer />
+
+        <button
+          id="back-to-top"
+          onclick="topFunction()"
+          class="fixed text-xl rounded-full z-10 bottom-5 end-5 h-8 w-8 text-center bg-white backdrop-blur-3xl text-slate-950 flex justify-center items-center transition-all duration-500 hover:bg-primary hover:text-white hover:rounded-lg"
+        >
+          <i data-lucide="chevron-up" class="h-4 w-4"></i>
+        </button>
+        {/* <!-- Back To Top End --> */}
+      </body>
+      {/* End of New design */}
+    </>
+  );
 }
 
 export default Skills;
